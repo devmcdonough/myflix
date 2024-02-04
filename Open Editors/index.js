dcfb4462d47015c592ee19da3a53dd395cac10aa
devmcdonough <devmcdonough@gmail.com>
2024-02-03 21:03:55 -0500

    Exercise 2.4 - Middleware Added

--- Open Editors/index.js
+++ Open Editors/index.js
@@ -1,3 +1,77 @@
 const express = require('express');
+const morgan = require('morgan');
+    morgan = require('morgan');
 const app = express();
 
+// Logging
+app.use(morgan('common'));
+
+// Movie Json
+let tenMovies = [
+    {
+        title: Under_the_Silver_Lake,
+        genre: Mystery
+    },
+    {
+        title: Hellraiser,
+        genre: Horror
+    },
+    {
+        title: Rear_Window,
+        genre: Mystery
+    },
+    {
+        title: Memories_of_Murder,
+        genre: Mystery
+    },
+    {
+        title: Baraka,
+        genre: Documentary
+    },
+    {
+        title: Zodiac,
+        genre: Mystery
+    },
+    {
+        title: Total_Recall,
+        genre: Action
+    },
+    {
+        title: Thief,
+        genre: Thriller
+    },
+    {
+        title: Mulholland_Drive,
+        genre: Mystery
+    },
+    {
+        title: Goodfellas,
+        genre: Crime
+    }
+]
+
+
+
+// Route to home
+app.get('/', (req, res) => {
+    res.send('Where are all the movies about sand? Our finest material.');
+});
+
+// Route to json
+app.get('/movies', (req, res, next) => {
+    res.json(tenMovies);
+    next();
+});
+
+app.use('/documentation', express.static('public'));
+
+// Error handling
+app.use((err, req, res, next) => {
+   console.error(err.stack);
+   res.status(500).send('Oh no!'); 
+});
+
+// puts it on 8080
+app.listen(8080, () => {
+    console.log('My node test in on 8080');
+});
