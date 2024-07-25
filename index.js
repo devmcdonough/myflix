b798e550aefdfdc8d117957ecb0daaee58a3fe41
devmcdonough <devmcdonough@gmail.com>
2024-07-24 22:06:36 -0400

    Re-added routes to index.js

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(morgan('common'));
 
 const Movies = Models.Movie;
 const Users = Models.User;
+const Genres = Models.Genre;
+const Directors = Models.Director;
 
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -109,7 +111,203 @@ app.get('/movies', passport.authenticate('jwt', { session: false }), async (req,
     });
 });
 
-// Other routes...
+/**
+ * GET: Returns data about a single movie by title.
+ * @param {string} title - The title of the movie.
+ * @param {object} req - The request object.
+ * @param {object} res - The response object.
+ */
+app.get('/movies/title/:title', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  await Movies.findOne({ Title: req.params.title })
+    .then((movie) => {
+      res.status(201).json(movie);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+
+/**
+ * GET: Returns data about a genre by name.
+ * @param {string} name - The name of the genre.
+ * @param {object} req - The request object.
+ * @param {object} res - The response object.
+ */
+app.get('/genres/:name', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  await Genres.findOne({ Name: req.params.name })
+    .then((genre) => {
+      res.status(201).json(genre);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+
+/**
+ * GET: Returns data about a director by name.
+ * @param {string} name - The name of the director.
+ * @param {object} req - The request object.
+ * @param {object} res - The response object.
+ */
+app.get('/directors/:name', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  await Directors.findOne({ Name: req.params.name })
+    .then((director) => {
+      res.status(201).json(director);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+
+/**
+ * GET: Returns a list of all users.
+ * @param {object} req - The request object.
+ * @param {object} res - The response object.
+ */
+app.get('/users', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  await Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+/**
+ * GET: Returns data about a user by username.
+ * @param {string} username - The username of the user.
+ * @param {object} req - The request object.
+ * @param {object} res - The response object.
+ */
+app.get('/users/:username', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  await Users.findOne({ Username: req.params.username })
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+
+/**
+ * PUT: Updates a user's information.
+ * @param {string} username - The username of the user.
+ * @param {object} req - The request object.
+ * @param {object} res - The response object.
+ */
+app.put('/users/:username', [
+  check('Username', 'Username is required').isLength({ min: 5 }),
+  check('Username', 'Username contains non-alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()
+], passport.authenticate('jwt', { session: false }), async (req, res) => {
+  let errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  if (req.user.Username !== req.params.username) {
+    return res.status(400).send('Permission denied');
+  }
+
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  await Users.findOneAndUpdate({ Username: req.params.username }, {
+    $set: {
+      Username: req.body.Username,
+      Password: hashedPassword,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  }, { new: true })
+    .then((updatedUser) => {
+      res.status(201).json(updatedUser);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+
+/**
+ * POST: Adds a movie to a user's list of favorites.
+ * @param {string} username - The username of the user.
+ * @param {string} movieId - The ID of the movie.
+ * @param {object} req - The request object.
+ * @param {object} res - The response object.
+ */
+app.post('/users/:username/movies/:movieId', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  await Users.findOneAndUpdate({ Username: req.params.username }, {
+    $push: { FavoriteMovies: req.params.movieId }
+  }, { new: true })
+    .then((updatedUser) => {
+      res.status(201).json(updatedUser);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+
+/**
+ * DELETE: Removes a movie from a user's list of favorites.
+ * @param {string} username - The username of the user.
+ * @param {string} movieId - The ID of the movie.
+ * @param {object} req - The request object.
+ * @param {object} res - The response object.
+ */
+app.delete('/users/:username/movies/:movieId', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  await Users.findOneAndUpdate({ Username: req.params.username }, {
+    $pull: { FavoriteMovies: req.params.movieId }
+  }, { new: true })
+    .then(updatedUser => {
+      if (updatedUser) {
+        res.status(201).json(updatedUser);
+      } else {
+        res.status(404).send('Not found');
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+/**
+ * DELETE: Deletes a user by username.
+ * @param {string} username - The username of the user.
+ * @param {object} req - The request object.
+ * @param {object} res - The response object.
+ */
+app.delete('/users/:username', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  await Users.findOneAndDelete({ Username: req.params.username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.username + ' was not found');
+      } else {
+        res.status(200).send(req.params.username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Serve static documentation
+app.use('/documentation', express.static('public'));
+
+// Error-handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Oh no!');
+});
 
 /**
  * Starts the server on the specified port.
